cbff4d6a9a96c4cfc38c432fabdba8f6c35743d4
Ilya <kriushenkovi@gmail.com>
2020-12-02 13:50:28 +0300

    Added pdf

--- lab6/1.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001
-mkdir c:\lab6
-cd /d c:\lab6
-ver > ver.txt
-systeminfo | find "Virtual Memory: Available:" > system_info_find1.txt
-systeminfo | find "Virtual Memory: In Use:" > system_info_find2.txt
-echo list volume | diskpart | more +7 > diskpart.txt
