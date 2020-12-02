22858a800bcd1c9597b52459ec548874d475f8d4
Ilya <kriushenkovi@gmail.com>
2020-12-02 13:58:48 +0300

    Added 5 lab

---
+++ lab5/mem.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+array=()
+i=0
+while true
+do
+    array+=(0 1 2 3 4)
+    i=$(((i + 1) % 10000))
+    if [[ $i -eq 0 ]]
+    then
+        echo ${#array[*]}
+    fi
+done > report.log
